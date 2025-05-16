ac3af25509793fb9d6b563527d1273cfd260dbe6
root <root@srv560738.hstgr.cloud>
2025-05-16 19:04:03 +0000

    remove the remove of 42 display

--- src/kernel.c
+++ src/kernel.c
@@ -3,19 +3,10 @@
 void kernel_main(void)
 {
 	init_screen();
-	int welcome_logo = 1;
-  int nb = 10;
-	while (1)
-	{
-		if (is_ps2_data_ready())
-		{
-			if (welcome_logo == 1 && nb == 0)
-			{
-				welcome_logo = 0;
-				remove_logo();
-			}
-    nb--
-			handle_scancode();
-		}
-	}
+
+  while (1)
+  {
+    if (is_ps2_data_ready())
+      handle_scancode();
+  }
 }
